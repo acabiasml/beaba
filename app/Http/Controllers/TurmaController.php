d9ab524d218725e9c8105892469d4d33f12b82eb
Acabias Marques Luiz <acabiasml@gmail.com>
2022-07-26 14:58:29 -0400

    Teste

--- app/Http/Controllers/TurmaController.php
+++ app/Http/Controllers/TurmaController.php
@@ -8,11 +8,6 @@ use App\Http\Requests\UpdateTurmaRequest;
 
 class TurmaController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         //
