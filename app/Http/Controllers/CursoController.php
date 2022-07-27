009c60015b5775f3d80622439c62b89c0da83050
Acabias Marques Luiz <acabiasml@gmail.com>
2022-07-27 17:08:14 -0400

    Alterações

--- app/Http/Controllers/CursoController.php
+++ app/Http/Controllers/CursoController.php
@@ -8,32 +8,16 @@ use App\Http\Requests\UpdateCursoRequest;
 
 class CursoController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreCursoRequest  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(StoreCursoRequest $request)
     {
         //
