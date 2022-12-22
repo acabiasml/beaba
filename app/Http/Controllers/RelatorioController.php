7b55e5fae352dd10e84b7f145a16745e2a0bab26
Acabias Marques Luiz <acabiasml@gmail.com>
2022-12-22 18:37:44 -0400

    energia

--- app/Http/Controllers/RelatorioController.php
+++ app/Http/Controllers/RelatorioController.php
@@ -6,7 +6,9 @@ use App\Models\Area;
 use App\Models\Calendario;
 use App\Models\Componente;
 use App\Models\Curso;
+use App\Models\Diario;
 use App\Models\Escola;
+use App\Models\Frequencia;
 use App\Models\Media;
 use App\Models\Periodo;
 use App\Models\Turma;
@@ -35,73 +37,145 @@ class RelatorioController extends Controller
         $componentes = Componente::where("cursos_id", $curso->id)->get();
         $areas = Area::whereIn("id", Componente::where("cursos_id", $curso->id)->pluck("area_id")->unique())->get();
         $turma = Turma::where("cursos_id", $curso->id)->get();
+      
 
-        $boletim["curso-id"] = $curso->id;
-        $boletim["curso-nome"] = $curso->nome;
-        $boletim["curso-modalidade"] = $curso->modalidade;
+        if($request->opcao == "boletim" || $request->opcao == "boletimconc"){
 
-        $boletim["periodos"] = array();
-        
-        foreach($periodos as $periodo){
+            $boletim["curso-id"] = $curso->id;
+            $boletim["curso-nome"] = $curso->nome;
+            $boletim["curso-modalidade"] = $curso->modalidade;
+
+            $boletim["periodos"] = array();
             
-            $esteperiodo = array();
-            $esteperiodo["periodo-id"] = $periodo->id;
-            $esteperiodo["periodo-nome"] = $periodo->nome;
-
-            $esteperiodo["alunos"] = array();
-
-            foreach($turma as $matricula){
-
-                if($matricula->datamatricula <= $periodo->fim){
-                    if($matricula->datatransf == null || $matricula->datatransf > $periodo->inicio){
-                        $estealuno = array();
-                        $estealuno["aluno-id"] = $matricula->users_id;
-                        $estealuno["aluno-nome"] = $matricula->aluno;
-
-                        $estealuno["areas"] = array();
-                        foreach($areas as $area){
-                            $estaarea = array();
-                            $estaarea["area-id"] = $area->id;
-                            $estaarea["area-nome"] = $area->nome;
-
-                            $estaarea["componentes"] = array();
-
-                            foreach($componentes as $componente){
-                                if($componente->area_id == $area->id){
-                                    $estecomponente = array();
-                                    $estecomponente["componente-id"] = $componente->id;
-                                    $estecomponente["componente-nome"] = $componente->nome;
-
-                                    $amedia = Media::where("componentes_id", $componente->id)
-                                                    ->where("users_id", $matricula->users_id)
-                                                    ->where("periodos_id", $periodo->id)->first();
-
-                                    if($amedia == null){
-                                        $estecomponente["componente-media"] = "-";
-                                    }else{
-                                        $estecomponente["componente-media"] = $amedia->nota;
-                                    }
-
-                                    array_push($estaarea["componentes"], $estecomponente);
-                                }
-                            }
-
-                            array_push($estealuno["areas"], $estaarea);
-                        }
+            foreach($periodos as $periodo){
                 
-                        array_push($esteperiodo["alunos"], $estealuno);
+                $esteperiodo = array();
+                $esteperiodo["periodo-id"] = $periodo->id;
+                $esteperiodo["periodo-nome"] = $periodo->nome;
+
+                $esteperiodo["alunos"] = array();
+
+                foreach($turma as $matricula){
+
+                    if($matricula->datamatricula <= $periodo->fim){
+                        if($matricula->datatransf == null || $matricula->datatransf > $periodo->inicio){
+                            $estealuno = array();
+                            $estealuno["aluno-id"] = $matricula->users_id;
+                            $estealuno["aluno-nome"] = $matricula->aluno;
+
+                            $estealuno["areas"] = array();
+                            foreach($areas as $area){
+                                $estaarea = array();
+                                $estaarea["area-id"] = $area->id;
+                                $estaarea["area-nome"] = $area->nome;
+
+                                $estaarea["componentes"] = array();
+
+                                foreach($componentes as $componente){
+                                    if($componente->area_id == $area->id){
+                                        $estecomponente = array();
+                                        $estecomponente["componente-id"] = $componente->id;
+                                        $estecomponente["componente-nome"] = $componente->nome;
+
+                                        $amedia = Media::where("componentes_id", $componente->id)
+                                                        ->where("users_id", $matricula->users_id)
+                                                        ->where("periodos_id", $periodo->id)->first();
+
+                                        if($amedia == null){
+                                            $estecomponente["componente-media"] = "-";
+                                        }else{
+                                            $estecomponente["componente-media"] = $amedia->nota;
+                                        }
+
+                                        array_push($estaarea["componentes"], $estecomponente);
+                                    }
+                                }
+
+                                array_push($estealuno["areas"], $estaarea);
+                            }
+                    
+                            array_push($esteperiodo["alunos"], $estealuno);
+                        }
                     }
                 }
+
+                array_push($boletim["periodos"], $esteperiodo);
             }
 
-            array_push($boletim["periodos"], $esteperiodo);
-        }
-
-        if($request->opcao == "boletim"){
-            $arquivo = $pdf->loadView("relatorio.turboletim", ["dados" => $boletim, "curso" => $curso])->setPaper('a4', 'landscape');
-        }else if($request->opcao == "boletimconc"){
-            $arquivo = $pdf->loadView("relatorio.turboletimconc", ["dados" => $boletim, "curso" => $curso])->setPaper('a4', 'landscape');
+            if($request->opcao == "boletim"){
+                $arquivo = $pdf->loadView("relatorio.turboletim", ["dados" => $boletim, "curso" => $curso])->setPaper('a4', 'landscape');
+            }else{
+                $arquivo = $pdf->loadView("relatorio.turboletimconc", ["dados" => $boletim, "curso" => $curso])->setPaper('a4', 'landscape');
+            }
+            
         }else if($request->opcao == "individual"){
+
+            $individual = array();
+            foreach ($turma as $matricula){
+                
+                $estudante = User::where("id", $matricula->users_id)->first();
+                
+                $estealuno = array();
+                $estealuno["aluno"] = $estudante;
+
+                $estealuno["areas"] = array();
+                foreach($areas as $area){
+                    $estaarea["nome"] = $area->nome;
+                    
+                    $estaarea["componentes"] = array();
+                    foreach($componentes as $componente){
+                        if($componente->area_id == $area->id){
+                            $estecomponente["nome"] = $componente->nome;
+                            
+                            $estecomponente["notas"] = array();
+                            $media = 0;
+                            $totalfaltas = 0;
+                            $contador = 0;
+
+                            foreach($periodos as $periodo){
+                                $esteperiodo["nome"] = $periodo->nome;
+                                
+                                $consulta = Media::where("users_id", $matricula->users_id)
+                                    ->where("componentes_id", $componente->id)
+                                    ->where("periodos_id", $periodo->id)->first();
+                    
+                                if($consulta == null){
+                                    $esteperiodo["periodo-media"] = "-";
+                                }else{
+                                    $esteperiodo["periodo-media"] = $consulta->nota;
+                                    $media = $media + $consulta->nota;
+                                    $contador = $contador + 1;
+                                }
+
+                                $diasbimestre = Diario::where("componentes_id", $componente->id)
+                                                        ->where("data", ">=", $periodo->inicio)
+                                                        ->where("data", "<=", $periodo->fim)
+                                                        ->get();
+
+                                $esteperiodo["periodo-faltas"] = 0;
+                                foreach($diasbimestre as $dia){
+
+                                    $verifica = Frequencia::where("users_id", $matricula->users_id)
+                                                        ->where("diarios_id", $dia->id)->first();
+
+                                    if($verifica != null && $verifica->presenca == "F"){
+                                        $esteperiodo["periodo-faltas"] =  $esteperiodo["periodo-faltas"] + 1;
+                                    }
+                                }
+
+                                array_push($estecomponente["notas"], $esteperiodo);
+                            }
+                            array_push($estaarea["componentes"], $estecomponente);
+                        }
+                    }
+
+                    array_push($estealuno["areas"], $estaarea);
+                }
+
+                array_push($individual, $estealuno);
+            }
+
+            dd(json_encode($individual, JSON_PRETTY_PRINT));
 
         }else if($request->opcao == "matricula"){
 
