4edd987c284c9944ba10b9653f692b94beedfd3e
Acabias Marques Luiz <acabiasml@gmail.com>
2022-12-22 20:56:05 -0400

    Alterações no layout.

--- resources/views/diario/index.blade.php
+++ resources/views/diario/index.blade.php
@@ -14,18 +14,19 @@
     <table class="table table-responsive-sm">
     <thead>
         <tr>
-            <th class="text-uppercase text-primary text-xxs font-weight-bolder opacity-7" scope="col">Nome</th>
-            <th class="text-uppercase text-primary text-xxs font-weight-bolder opacity-7" scope="col">Horas</th>
-            <th class="text-uppercase text-primary text-xxs font-weight-bolder opacity-7" scope="col">Curso</th>
-            <th class="text-uppercase text-primary text-xxs font-weight-bolder opacity-7" scope="col">Lançamentos</th>
+            <th class="text-uppercase text-primary text-xxs font-weight-bolder opacity-7" style="text-align: center" scope="col">Nome</th>
+            <th class="text-uppercase text-primary text-xxs font-weight-bolder opacity-7" style="text-align: center" scope="col">Horas</th>
+            <th class="text-uppercase text-primary text-xxs font-weight-bolder opacity-7" style="text-align: center" scope="col">Curso</th>
+            <th class="text-uppercase text-primary text-xxs font-weight-bolder opacity-7" style="text-align: center" scope="col">Lançamentos</th>
+            <th class="text-uppercase text-primary text-xxs font-weight-bolder opacity-7" style="text-align: center" scope="col">Ações</th>
         </tr>
     </thead>
     <tbody>
         @foreach ($doProfessor as $key)
             <tr>
-                <td>{{$key["componente_nome"]}}</td>
-                <td>{{$key["componente_cumprido"]}}/{{$key["componente_horas"]}}</td>
-                <td>{{$key["componente_nome_curso"]}} - {{$key["componente_status_curso"]}}</td>
+                <td style="text-align: center">{{$key["componente_nome"]}}</td>
+                <td style="text-align: center">{{$key["componente_cumprido"]}}/{{$key["componente_horas"]}}</td>
+                <td style="text-align: center">{{$key["componente_nome_curso"]}} - {{$key["componente_status_curso"]}}</td>
                 @if ($key["componente_status_curso"] == "iniciado")
                     <td>
                         Período: 
@@ -42,7 +43,14 @@
                             <br/><br/>
                             <button class="btn btn-info btn-sm" type="submit" formaction="{{route('medias')}}">Notas</button>
                             <button class="btn btn-success btn-sm" type="submit" formaction="{{route('diario.ver')}}">Conteúdos e Frequência</button>
-                            <button class="btn btn-warning btn-sm" type="submit" formaction="{{route('diario.print')}}">Imprimir</button>
+                        </form>
+                    </td>
+
+                    <td  style="text-align: center">
+                        <form method="GET" action="{{route('diario.print')}}">
+                            @csrf
+                            <input type="hidden" name="componente" value="{{$key['componente_id']}}" />
+                            <button class="btn btn-warning btn-sm" type="submit">Imprimir</button>
                         </form>
                     </td>
                 @endif
